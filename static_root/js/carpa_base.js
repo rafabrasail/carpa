f96b960409fe54a349f5994abc4f94c9ffe7602b
rafabrasail <rafaelrosenberg89@gmail.com>
2021-12-31 08:55:36 -0300

    3d model implemented

--- static_root/js/carpa_base.js
+++ static_root/js/carpa_base.js
@@ -7,130 +7,89 @@
 // Do not touch here - code for render THREE into html tag
 
 import * as THREE from 'three';
-import GUI from 'lil-gui';
-import {OBJLoader} from 'three'
+// import GUI from 'lil-gui';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js';
+
+
+	// var container = document.getElementById('canvas')
+	// var renderer = new THREE.WebGLRenderer({ antialias: true });
+
+	// renderer.setSize( window.innerWidth, window.innerHeight );
+	// container.appendChild( renderer.domElement );
+	// const scene = new THREE.Scene();
+	// const gui = new GUI();
+
+  // const objLoader = new OBJLoader();
+
+  //  const canvas = document.querySelector('#canvas');
+  //  const canvas = document.getElementById('canvas')
+  //  const renderer = new THREE.WebGLRenderer({canvas});
 
 
 function main() {
-	var container = document.getElementById('canvas')
-	var renderer = new THREE.WebGLRenderer({ antialias: true });
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
-	const scene = new THREE.Scene();
-	const gui = new GUI();
-
-  const objLoader = new OBJLoader();
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
   const fov = 45;
   const aspect = 2;  // the canvas default
   const near = 0.1;
-  const far = 1000;
+  const far = 100;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 1000, 0);
-  camera.up.set(0, 0, 1);
-  camera.lookAt(0, 0, 0);
+  camera.position.set(0, 10, 20);
 
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('black');
 
   {
-    const color = 0xFFFFFF;
-    const intensity = 3;
-    const light = new THREE.PointLight(color, intensity);
+    const planeSize = 40;
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load("{% static 'img/checker.png' %}");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add(mesh);
+  }
+
+  {
+    const skyColor = 0xB1E1FF;  // light blue
+    const groundColor = 0xB97A20;  // brownish orange
+    const intensity = 1;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
     scene.add(light);
   }
 
-  // an array of objects who's rotation to update
-  const objects = [];
-
-
-const radius = 1;
-const widthSegments = 6;
-const heightSegments = 6;
-const sphereGeometry = new THREE.SphereGeometry(
-radius, widthSegments, heightSegments);
-
-
-const solarSystem = new THREE.Object3D();
-scene.add(solarSystem);
-objects.push(solarSystem);
-
-
-const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
-const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-sunMesh.scale.set(5, 5, 5);
-solarSystem.add(sunMesh);
-objects.push(sunMesh);
-
-
- const earthOrbit = new THREE.Object3D();
-earthOrbit.position.x = 10;
-solarSystem.add(earthOrbit);
-objects.push(earthOrbit);
-
-  
-const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
-const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-earthOrbit.add(earthMesh);
-objects.push(earthMesh); 
-
-
-const moonOrbit = new THREE.Object3D();
-moonOrbit.position.x = 2;
-earthOrbit.add(moonOrbit);
-
-
-const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
-const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-moonMesh.scale.set(.5, .5, .5);
-moonOrbit.add(moonMesh);
-objects.push(moonMesh);
-
-
-class AxisGridHelper {
-  constructor(node, units = 10) {
-    const axes = new THREE.AxesHelper();
-    axes.material.depthTest = false;
-    axes.renderOrder = 2;  // after the grid
-    node.add(axes);
- 
-    const grid = new THREE.GridHelper(units, units);
-    grid.material.depthTest = false;
-    grid.renderOrder = 1;
-    node.add(grid);
- 
-    this.grid = grid;
-    this.axes = axes;
-    this.visible = false;
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 10, 0);
+    light.target.position.set(-5, 0, 0);
+    scene.add(light);
+    scene.add(light.target);
   }
-  get visible() {
-    return this._visible;
+
+  {
+    const objLoader = new OBJLoader();
+    objLoader.load("{% static 'img/windmill.obj' %}", (root) => {
+      scene.add(root);
+    });
   }
-  set visible(v) {
-    this._visible = v;
-    this.grid.visible = v;
-    this.axes.visible = v;
-  }
-}
-
-
-// // add an AxesHelper to each node
-objects.forEach((node) => {
-  const axes = new THREE.AxesHelper();
-  axes.material.depthTest = false;
-  axes.renderOrder = 1;
-  node.add(axes);
-});
-
-function makeAxisGrid(node, label, units) {
-  const helper = new AxisGridHelper(node, units);
-  gui.add(helper, 'visible').name(label);
-}
- 
-makeAxisGrid(solarSystem, 'solarSystem', 25);
-makeAxisGrid(sunMesh, 'sunMesh');
-makeAxisGrid(earthOrbit, 'earthOrbit');
-makeAxisGrid(earthMesh, 'earthMesh');
-makeAxisGrid(moonOrbit, 'moonOrbit');
-makeAxisGrid(moonMesh, 'moonMesh');
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -143,18 +102,13 @@ makeAxisGrid(moonMesh, 'moonMesh');
     return needResize;
   }
 
-  function render(time) {
-    time *= 0.001;
+  function render() {
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
-
-    objects.forEach((obj) => {
-      obj.rotation.y = time;
-    });
 
     renderer.render(scene, camera);
 
